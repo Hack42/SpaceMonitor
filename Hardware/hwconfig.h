e0a5c39d1b9f669d32d5cdcda806f970e8095f4d
WitchDoc42 <witchdoc@hack42.nl>
2013-10-03 22:19:11 +0200

    Initial checkin

---
+++ Hardware/hwconfig.h
@@ -0,0 +1,124 @@
+#ifndef HWCONFIG_H
+#define HWCONFIG_H
+
+//-----------------------------------------------------------------
+//------------------------- DO NOT MODIFY -------------------------
+//------------------Generated with the generate_hearer.ulp---------
+//-----------------------------------------------------------------
+
+
+#define 1WIRE_PORT   PORTB
+#define 1WIRE_PINREG PINB
+#define 1WIRE_PIN    7
+#define 1WIRE_DDR    DDRB
+#define 1WIRE        B,7
+
+#define MISO_PORT   PORTB
+#define MISO_PINREG PINB
+#define MISO_PIN    4
+#define MISO_DDR    DDRB
+#define MISO        B,4
+
+#define MON-A-1_PORT   PORTC
+#define MON-A-1_PINREG PINC
+#define MON-A-1_PIN    0
+#define MON-A-1_DDR    DDRC
+#define MON-A-1        C,0
+
+#define MON-A-2_PORT   PORTC
+#define MON-A-2_PINREG PINC
+#define MON-A-2_PIN    1
+#define MON-A-2_DDR    DDRC
+#define MON-A-2        C,1
+
+#define MON-A-3_PORT   PORTC
+#define MON-A-3_PINREG PINC
+#define MON-A-3_PIN    2
+#define MON-A-3_DDR    DDRC
+#define MON-A-3        C,2
+
+#define MON-A-4_PORT   PORTC
+#define MON-A-4_PINREG PINC
+#define MON-A-4_PIN    3
+#define MON-A-4_DDR    DDRC
+#define MON-A-4        C,3
+
+#define MON-D-1_PORT   PORTD
+#define MON-D-1_PINREG PIND
+#define MON-D-1_PIN    7
+#define MON-D-1_DDR    DDRD
+#define MON-D-1        D,7
+
+#define MON-D-2_PORT   PORTD
+#define MON-D-2_PINREG PIND
+#define MON-D-2_PIN    6
+#define MON-D-2_DDR    DDRD
+#define MON-D-2        D,6
+
+#define MON-D-3_PORT   PORTD
+#define MON-D-3_PINREG PIND
+#define MON-D-3_PIN    5
+#define MON-D-3_DDR    DDRD
+#define MON-D-3        D,5
+
+#define MON-D-4_PORT   PORTD
+#define MON-D-4_PINREG PIND
+#define MON-D-4_PIN    4
+#define MON-D-4_DDR    DDRD
+#define MON-D-4        D,4
+
+#define MON-D-5_PORT   PORTD
+#define MON-D-5_PINREG PIND
+#define MON-D-5_PIN    3
+#define MON-D-5_DDR    DDRD
+#define MON-D-5        D,3
+
+#define MON-D-6_PORT   PORTD
+#define MON-D-6_PINREG PIND
+#define MON-D-6_PIN    2
+#define MON-D-6_DDR    DDRD
+#define MON-D-6        D,2
+
+#define MOSI_PORT   PORTB
+#define MOSI_PINREG PINB
+#define MOSI_PIN    3
+#define MOSI_DDR    DDRB
+#define MOSI        B,3
+
+#define OUT-CH-1_PORT   PORTB
+#define OUT-CH-1_PINREG PINB
+#define OUT-CH-1_PIN    1
+#define OUT-CH-1_DDR    DDRB
+#define OUT-CH-1        B,1
+
+#define OUT-CH-2_PORT   PORTB
+#define OUT-CH-2_PINREG PINB
+#define OUT-CH-2_PIN    2
+#define OUT-CH-2_DDR    DDRB
+#define OUT-CH-2        B,2
+
+#define RST_PORT   PORTC
+#define RST_PINREG PINC
+#define RST_PIN    6
+#define RST_DDR    DDRC
+#define RST        C,6
+
+#define RXD_PORT   PORTD
+#define RXD_PINREG PIND
+#define RXD_PIN    0
+#define RXD_DDR    DDRD
+#define RXD        D,0
+
+#define SCK_PORT   PORTB
+#define SCK_PINREG PINB
+#define SCK_PIN    5
+#define SCK_DDR    DDRB
+#define SCK        B,5
+
+#define TXD_PORT   PORTD
+#define TXD_PINREG PIND
+#define TXD_PIN    1
+#define TXD_DDR    DDRD
+#define TXD        D,1
+
+#endif //#define HWCONFIG_H
